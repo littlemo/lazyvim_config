875821685188cccddf96504d2444b1528b4e50e5
Moore.Huang <moore@moorehy.com>
2024-06-19 17:15:45 +0800

    feat(formatting): 更新LSP格式化配置，不再使用 none-ls 插件

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -2,16 +2,8 @@ return {
   {
     "williamboman/mason.nvim",
     opts = function(_, opts)
+      table.insert(opts.ensure_installed, "black")
       table.insert(opts.ensure_installed, "isort")
-    end,
-  },
-  {
-    "nvimtools/none-ls.nvim",
-    optional = true,
-    opts = function(_, opts)
-      local nls = require("null-ls")
-      opts.sources = opts.sources or {}
-      table.insert(opts.sources, nls.builtins.formatting.isort)
     end,
   },
   {
