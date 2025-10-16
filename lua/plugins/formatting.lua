9dd3f1dced2b8f01c40f173d0c30731b5a03f071
Moore.Huang <moore@moorehy.com>
2025-10-16 19:00:24 +0800

    feat(formatting): 修改 mason 包名

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,6 +1,6 @@
 return {
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     opts = function(_, opts)
       table.insert(opts.ensure_installed, "black")
       table.insert(opts.ensure_installed, "isort")
